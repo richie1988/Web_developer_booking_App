e18d593336b084188734c3dd2f018b3f61da1d56
richie1988 <richiessikaonga@gmail.com>
2024-01-29 12:09:47 +0200

    Add form filed implementation

--- react-front-end/src/components/AddDeveloperForm/index.jsx
+++ react-front-end/src/components/AddDeveloperForm/index.jsx
@@ -1,8 +1,6 @@
-// AddDeveloper.jsx
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addDeveloper } from '../../redux/actions/DevelopersActions';
-
 
 function AddDeveloper() {
   const dispatch = useDispatch();
@@ -19,21 +17,22 @@ function AddDeveloper() {
     setDeveloper({ ...developer, [e.target.name]: e.target.value });
   };
 
-  const handleAddDeveloper = () => {
+  const handleAddDeveloper = (e) => {
+    e.preventDefault();
     dispatch(addDeveloper(developer));
   };
 
   return (
     <div>
       <h1>Add Developer Page</h1>
-      <form>
-        {/* Add form fields for developer details */}
-        <button type="button" onClick={handleAddDeveloper}>
-          Add Developer
-        </button>
-        <button type="button" onClick={handleInputChange}>
-          Add Developer
-        </button>
+      <form onSubmit={handleAddDeveloper}>
+        <input type="text" name="fullName" value={developer.fullName} onChange={handleInputChange} placeholder="Full Name" />
+        <input type="text" name="phoneNumber" value={developer.phoneNumber} onChange={handleInputChange} placeholder="Phone Number" />
+        <input type="text" name="hourlyRate" value={developer.hourlyRate} onChange={handleInputChange} placeholder="Hourly Rate" />
+        <input type="text" name="city" value={developer.city} onChange={handleInputChange} placeholder="City" />
+        <input type="email" name="email" value={developer.email} onChange={handleInputChange} placeholder="Email" />
+        <input type="text" name="apr" value={developer.apr} onChange={handleInputChange} placeholder="APR" />
+        <button type="submit">Add Developer</button>
       </form>
     </div>
   );
