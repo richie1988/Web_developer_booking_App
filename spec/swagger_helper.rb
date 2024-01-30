2f0a28af5977de91bbfa0a588b97411eb891385f
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2024-01-30 19:57:19 +0100

    Solved linters

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-
   config.openapi_root = Rails.root.join('swagger').to_s
 
   config.openapi_specs = {
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
-      info: {
-        title: 'API V1',
-        version: 'v1'
-      },
+      info: { title: 'API V1', version: 'v1' },
       paths: {},
       components: {
         securitySchemes: {
