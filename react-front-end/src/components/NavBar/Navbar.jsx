6bc706dd0897cc398a1ebe7676d09f653b338a5a
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2024-02-02 15:06:11 +0500

    Amend the required changes

--- react-front-end/src/components/NavBar/Navbar.jsx
+++ react-front-end/src/components/NavBar/Navbar.jsx
@@ -1,33 +1,109 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaFacebookF, FaVimeoV, FaPinterestP } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa6";
-import { TiSocialGooglePlus } from "react-icons/ti";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FaFacebookF, FaVimeoV, FaPinterestP, FaTwitter, FaGooglePlusG } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineCopyrightCircle } from 'react-icons/ai';
+import { BsTwitter,  } from 'react-icons/bs';
+import './Navbar.css';
 
 function Navbar() {
+  const [isNavVisible, setNavVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setNavVisible(!isNavVisible);
+  };
+
   return (
-    <>
-    <div className="container-Navbar">
-      <div className="header">
-        <h1 className="heading-title">Web Developer</h1>
-        <img className="logo-icon" src="https://th.bing.com/th/id/OIG4.e2TdRPOPLhsW0eZMF6_J?w=1024&h=1024&rs=1&pid=ImgDetMain" alt="Logo" />
+    <div className={`nav-container ${isNavVisible ? 'open' : ''} d-flex flex-column align-items-center`}>
+      <nav className="burger">
+        <button type="button" aria-label="Navigation" className="toggle-button" onClick={toggleSidebar}>
+          <GiHamburgerMenu />
+        </button>
+      </nav>
+
+      <nav className={`nav ${isNavVisible ? 'show' : 'hidden'}`}>
+        {/* <div className="logo-continer"> */}
+          <img className="logo-img" src="https://th.bing.com/th/id/OIG4.e2TdRPOPLhsW0eZMF6_J?w=1024&h=1024&rs=1&pid=ImgDetMain" alt="logo" />
+        {/* </div> */}
+        <ul className="sidenav-list d-flex flex-column justify-content-center">
+          <li className="sidenav-item">
+            <NavLink to="/LandingPage" className="sidenav-link" onClick={toggleSidebar}>
+              Home
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/Login" className="sidenav-link" onClick={() => { toggleSidebar(); }}>
+            Login
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/Signup" className="sidenav-link" onClick={() => { toggleSidebar(); }}>
+              Sign Up
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/AddDeveloper" className="sidenav-link" onClick={() => { toggleSidebar(); }}>
+            Add Developer
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/DeleteDeveloper" className="sidenav-link" onClick={() => { toggleSidebar(); }}>
+            Remove Developer
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/MyReservations" className="sidenav-link" onClick={toggleSidebar}>
+              My Reservations
+            </NavLink>
+          </li>
+          <li className="sidenav-item">
+            <NavLink to="/newreservation" className="sidenav-link" onClick={toggleSidebar}>
+              New Reservation
+            </NavLink>
+          </li>
+        </ul>
+        <div className="sidenav-footer">
+          <ul className="sidebar-socials">
+          <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <BsTwitter />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaFacebookF />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaGooglePlusG />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaVimeoV />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaPinterestP />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="logo-link" onClick={toggleSidebar}>
+                <FaTwitter />
+              </NavLink>
+            </li>
+          </ul>
+          <p className="copyright">
+            <span><AiOutlineCopyrightCircle /></span>
+            2024 Web Developers Own
+          </p>
         </div>
-        <div className="links-navbar">
-        <Link to="/LandingPage">Home</Link>
-        <Link to="/Login">Login</Link>
-        <Link to="/Signup">Sign Up</Link> 
-      </div>
-      <div className="footer">
-          <FaTwitter/>
-          <FaFacebookF/>
-          <TiSocialGooglePlus/>
-          <FaVimeoV/>
-          <FaPinterestP/>
-      </div>
-      </div>
-    </>
-  )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
