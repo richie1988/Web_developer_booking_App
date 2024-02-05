2c82e2cd0bbf453b31f08217dc730f6c35dc0677
richie1988 <richiessikaonga@gmail.com>
2024-02-05 19:10:50 +0200

    made files seperation

---
+++ app/javascript/src/components/NavbarDemo/index.jsx
@@ -0,0 +1,37 @@
+// Sidebar.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <div className="menu-toggle" onClick={toggleSidebar}>
+        {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </div>
+      <div className="menu-items">
+        <Link to="/developersList" onClick={toggleSidebar}>Developers List</Link>
+        <Link to="/add-developers" onClick={toggleSidebar}>Add Developer</Link>
+        <Link to="/developerDetails" onClick={toggleSidebar}>Delete Developer</Link>
+        <Link to="/my-reservations" onClick={toggleSidebar}>My Reservations</Link>
+        <Link to="/add-reservation" onClick={toggleSidebar}>Add Reservation</Link>
+        <Link to="/logout" onClick={toggleSidebar}>Logout</Link>
+      </div>
+      <div className="social-icons">
+        <FaFacebook />
+        <FaTwitter />
+        <FaInstagram />
+        <FaLinkedin />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
-import DeveloperList from './components/DeveloperList';
+import Developers from './components/DeveloperListCard';
 import DeveloperDetails from './components/DeveloperDetails';
 import ReservationForm from './components/ReservationForm';
 import MyReservations from './components/MyReservations';
@@ -12,16 +12,16 @@ import DeleteDeveloperList from './components/DeleteDeveloperList';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Navbar from './components/NavBar/Navbar';
+import Navbar from './components/NavbarDemo';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<WelcomePage />} /> */}
+          <Route path="/" element={<WelcomePage />} />
           <Route
             path="/*"
             element={
@@ -30,7 +30,7 @@ function App() {
                 <div className="content-container">
                   <Routes>
                     <Route index element={<HomePage />} />
-                    <Route path="/developersList" element={<DeveloperList />} />
+                    <Route path="/developers" element={<Developers />} />
                     <Route path="/add-developers" element={<AddDeveloperForm />} />
                     <Route
                       path="/delete-developer/:id"
