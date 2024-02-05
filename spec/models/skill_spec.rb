39172929ffb6e18777b537998118368334a35aa9
Samuel wanza <s.wanza@alustudent.com>
2024-02-05 06:22:47 +0200

    added webdeveloper and skill model testing

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
-
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Skill.new(name: 'Rails') }
+  before { subject.save }
+  it 'name should be present have some inclusion' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
 end
--- spec/models/webdeveloper_skill_spec.rb
+++ spec/models/webdeveloper_skill_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe WebdeveloperSkill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  @skill = Skill.create(name: 'React')
+  @webdeveloper=WebDeveloper.create(name: 'Kranko',title:"Frontend Developer",description:"A multi talented developer with 4 years of experience",hourly_rate:50,image_url:"https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/06/person-at-computer.jpeg.jpg",
+  linkedin_url:"https://www.linkedin.com/in/samuel-munguti/",twitter_url:"https://twitter.com/samuelmunguti9",
+  github_url:"https://github.com/Samuelwanza",city:"kigali",phone:"+254768331257",email:"example3@gmail.com")
+  subject { WebdeveloperSkill.new(level:5, web_developer_id:@webdeveloper, skill_id:@skill) }
+  before { subject.save }
+  it 'level should be present have some inclusion' do
+    subject.level = nil
+    expect(subject).to_not be_valid
+  end
+
 end
