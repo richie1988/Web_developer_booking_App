286fbcad24e4b92511f4ebe03812f9eb4499902e
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2024-02-08 09:19:56 +0500

    initialized repo and removed conflicts

--- app/javascript/components/ReservationForm/index.jsx
+++ app/javascript/components/ReservationForm/index.jsx
@@ -1,37 +1,135 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addReservation } from '../../redux/actions/ReservationsActions';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { useLocation } from 'react-router';
+import { createReservation } from '../../redux/reservation/thunk';
+import { fetchDevelopers } from '../../redux/Developer/DeveloperSlice';
+import { selectStatus } from '../../redux/user/userSlice';
+import Sidenav from '../Navbar/Sidenav';
 
-function ReservationForm() {
+const NewReservation = () => {
   const dispatch = useDispatch();
-  const [reservation, setReservation] = useState({
-    developerFullName: '',
-    developerPhoneNumber: '',
-    date: '',
-    duration: '',
-  });
+  const Developers = useSelector((state) => state.Developer.Developers);
+  const location = useLocation();
+  const selectedDeveloper = location.state;
+  const userStatus = useSelector(selectStatus);
 
-  const handleInputChange = (e) => {
-    setReservation({ ...reservation, [e.target.name]: e.target.value });
-  };
+  const userId = JSON.parse(localStorage.getItem('user_id'));
+  console.log('reservation user:', userId);
+  const [date, setDate] = useState('');
+  // const [city, setCity] = useState('');
+  const [DeveloperId, setDeveloperId] = useState(selectedDeveloper ? selectedDeveloper.id : '');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleAddReservation = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(addReservation(reservation));
+
+    if (!userId) {
+      toast.error('User ID is not available. Please login and try again.');
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const reservationData = {
+        user_id: userId,
+        Developer_id: DeveloperId,
+        // city,
+        date,
+      };
+
+      await dispatch(createReservation({ data: reservationData }));
+      setDeveloperId('');
+      // setCity('');
+      setDate('');
+      toast.success('Developer added successfully');
+    } catch (err) {
+      setError('Error creating a reservation. Please try again later.');
+      toast.error('Please fill all fields before you submit');
+    }
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    // if (userStatus === 'idle' && userId) {
+    dispatch(fetchDevelopers());
+    console.log('Developers:', Developers);
+    // }
+  }, [dispatch, userStatus, userId]);
 
   return (
-    <div>
-      <h1>Reservation Form</h1>
-      <form onSubmit={handleAddReservation}>
-        <input type="text" name="developerFullName" value={reservation.developerFullName} onChange={handleInputChange} placeholder="Developer Full Name" />
-        <input type="text" name="developerPhoneNumber" value={reservation.developerPhoneNumber} onChange={handleInputChange} placeholder="Developer Phone Number" />
-        <input type="date" name="date" value={reservation.date} onChange={handleInputChange} />
-        <input type="number" name="duration" value={reservation.duration} onChange={handleInputChange} placeholder="Duration" />
-        <button type="submit">Make Reservation</button>
-      </form>
-    </div>
-  );
-}
+    <>
+      <Sidenav />
+      <div className="bg-lime-400 px-10  h-screen pt-[1rem]  md:pl-[14rem] md:pt-[8rem]">
 
-export default ReservationForm;
+        {/* Scheduling Information */}
+        <div className="text-center text-white md:mx-auto p-1 md:pl-24 md:p-8">
+          <h2 className="font-bold">Reservation Schedule Information</h2>
+          <p className="text-center">
+            Our Developers are available from Monday to Friday,
+            9:00 AM to 5:00 PM. If you have any questions
+            <br />
+            or need assistance,feel free to contact our support team at support@example.com.
+          </p>
+        </div>
+        <div className="mx-auto max-w-screen-md">
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:w-[80%] justify-center items-center md:pl-[6rem]">
+            {/* Date Input */}
+            <div className="flex flex-col w-full  md:ml-6">
+              <p className="text-lg font-medium text-white">
+                Date
+              </p>
+              <input
+                id="date"
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                className="p-2 border rounded reserve-input"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full  md:ml-9">
+              <p className="text-md font-medium text-white">
+                Choose Developer
+              </p>
+              <select
+                id="Developer"
+                onChange={(e) => setDeveloperId(e.target.value)}
+                value={DeveloperId}
+                className="p-2 border rounded reserve-select"
+                required
+              >
+                <option value="">Select Developer</option>
+                {Developers.map((Developer) => (
+                  <option key={Developer.id} value={Developer.id}>
+                    {Developer.first_name}
+                  </option>
+                ))}
+              </select>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className={`reserve-btn bg-gray-200 px-3 w-40 h-11 rounded mt-5 text-lime-500 font-[400] hover:bg-lime-500 hover:text-white ${
+                    isLoading ? 'cursor-not-allowed' : ''
+                  }`}
+                  disabled={isLoading}
+                >
+                  {isLoading ? 'Reserving...' : 'Reserve'}
+                </button>
+              </div>
+            </div>
+
+            {/* Error Message */}
+            {error && <p className="text-red-500">{error}</p>}
+
+            {/* Submit Button */}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewReservation;
